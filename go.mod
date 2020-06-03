3a9e3e9dd0c5bf51c897bb5c776cfac17065216a
Feodor Fitsner <feodor@appveyor.com>
2020-06-03 15:45:17 -0700

    go API server added

---
+++ go.mod
@@ -0,0 +1,8 @@
+module appveyor.com/pglet/pglet
+
+go 1.14
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.6.3
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./client/build", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	// Start and run the server
+	router.Run(":5000")
+}
