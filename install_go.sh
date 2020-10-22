8d753c9c322dd024bb684d273004e21fb1307177
Feodor Fitsner <feodor@appveyor.com>
2020-10-22 11:46:59 -0700

    Download go to a temp dir

--- install_go.sh
+++ install_go.sh
@@ -3,8 +3,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     GO_OS=darwin
 fi
 
-curl -fsSL "https://golang.org/dl/go${GO_VERSION}.${GO_OS}-amd64.tar.gz" -o go-linux-amd64.tar.gz
-sudo tar zxf go-linux-amd64.tar.gz -C /usr/local
+curl -fsSL "https://golang.org/dl/go${GO_VERSION}.${GO_OS}-amd64.tar.gz" -o /tmp/go-linux-amd64.tar.gz
+sudo tar zxf /tmp/go-linux-amd64.tar.gz -C /usr/local
 export PATH=/usr/local/go/bin:$PATH
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
