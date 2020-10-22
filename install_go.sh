0e05bb79eaf736df87cbe609161979f1f6d9601f
Feodor Fitsner <feodor@appveyor.com>
2020-10-22 11:31:09 -0700

    Replace wget with curl

--- install_go.sh
+++ install_go.sh
@@ -1,5 +1,5 @@
-wget -q -O dotnet-sdk.tar.gz https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
-sudo tar zxf dotnet-sdk.tar.gz -C /usr/local
+curl -fsSL "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go-linux-amd64.tar.gz
+sudo tar zxf go-linux-amd64.tar.gz -C /usr/local
 export PATH=/usr/local/go/bin:$PATH
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
