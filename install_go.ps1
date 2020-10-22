acd7cc92c0bc589dad1119f63afe7bf3b5e107fc
Feodor Fitsner <feodor@appveyor.com>
2020-10-22 11:15:52 -0700

    Fix install_go.ps1

--- install_go.ps1
+++ install_go.ps1
@@ -1,7 +1,7 @@
 Remove-Item 'C:\go' -Recurse -Force
 
 $goDistPath = "$env:TEMP\go$GO_VERSION.windows-amd64.zip"
-(New-Object Net.WebClient).DownloadFile("https://dl.google.com/go/go$GO_VERSION.windows-amd64.zip", $goDistPath)
+(New-Object Net.WebClient).DownloadFile("https://golang.org/dl/go$GO_VERSION.windows-amd64.zip", $goDistPath)
 
 Write-Host "Unpacking..."
 7z x $goDistPath -o"$env:SystemDrive\" | Out-Null
