880ff9a28ae3f6b8e29ea4eadf1df194d3b6b374
Feodor Fitsner <feodor@appveyor.com>
2020-10-22 11:28:28 -0700

    Add macOS build

--- install_go.ps1
+++ install_go.ps1
@@ -2,8 +2,6 @@ Remove-Item 'C:\go' -Recurse -Force
 
 $goDistPath = "$env:TEMP\go$GO_VERSION.windows-amd64.zip"
 (New-Object Net.WebClient).DownloadFile("https://golang.org/dl/go$($env:GO_VERSION).windows-amd64.zip", $goDistPath)
-
-Write-Host "Unpacking..."
 7z x $goDistPath -o"$env:SystemDrive\" | Out-Null
 
 go version
