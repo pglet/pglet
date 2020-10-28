51b3ca254020cfa4d72f792da455384b4e5f1e6e
Feodor Fitsner <feodor@appveyor.com>
2020-10-27 22:06:24 -0700

    Global log-level arg

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -3,12 +3,14 @@ package commands
 import (
 	"fmt"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var (
-	version = "unknown"
-	commit  = "unknown"
+	version  = "unknown"
+	commit   = "unknown"
+	LogLevel string
 )
 
 func NewRootCmd() *cobra.Command {
@@ -16,9 +18,22 @@ func NewRootCmd() *cobra.Command {
 		Use:     "pglet",
 		Short:   "Pglet",
 		Version: fmt.Sprintf("%s-%s", version, commit),
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
+			level := log.FatalLevel // default logging level
+			level, err := log.ParseLevel(LogLevel)
+
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			log.SetLevel(level)
+		},
 	}
 
 	cmd.SetVersionTemplate("{{.Version}}")
+
+	cmd.PersistentFlags().StringVarP(&LogLevel, "log-level", "l", "info", "verbosity level for logs")
 
 	cmd.AddCommand(
 		newPageCommand(),
