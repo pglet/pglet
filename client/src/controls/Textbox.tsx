0ba182d6752f3e326cad7f20a036c4893af18723
Feodor Fitsner <feodor@appveyor.com>
2020-12-07 20:03:04 -0800

    Password control

--- client/src/controls/Textbox.tsx
+++ client/src/controls/Textbox.tsx
@@ -33,12 +33,15 @@ export const Textbox = React.memo<IControlProps>(({control}) => {
     errorMessage: control.errormessage ? control.errormessage : null,
     description: control.description ? control.description : null,
     multiline: control.multiline ? true : false,
+    type: control.password ? "password" : undefined,
+    canRevealPassword: control.password ? true : undefined,
+    required: control.required ? true : undefined,
     styles: {
       root: {
         width: control.width !== undefined ? control.width : undefined,
         height: control.height !== undefined ? control.height : undefined,
         padding: control.padding !== undefined ? control.padding : undefined,
-        margin: control.margin !== undefined ? control.margin : undefined        
+        margin: control.margin !== undefined ? control.margin : undefined  
       }
     }
   };
