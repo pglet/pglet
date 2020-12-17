848c8738d1f8218c15fbd2d30a6a7fcd9f4fbe5a
Feodor Fitsner <feodor@appveyor.com>
2020-12-17 11:15:41 -0800

    Textbox fixed

--- client/src/controls/Textbox.tsx
+++ client/src/controls/Textbox.tsx
@@ -2,13 +2,15 @@ import React, { useContext } from 'react';
 import { WebSocketContext } from '../WebSocket';
 import { useDispatch } from 'react-redux'
 import { changeProps } from '../slices/pageSlice'
-import { TextField } from 'office-ui-fabric-react';
+import { TextField, ITextFieldProps } from '@fluentui/react';
+import { IControlProps } from './IControlProps'
 
-const Textbox = React.memo<ImStatelessProps>(({control}) => {
+export const Textbox = React.memo<IControlProps>(({control, parentDisabled}) => {
 
-  console.log(`render Textbox: ${control.i}`);
+  //console.log(`render Textbox: ${control.i}`);
+  let disabled = (control.disabled === 'true') || parentDisabled;
 
-  const ws = useContext<any>(WebSocketContext);
+  const ws = useContext(WebSocketContext);
 
   const dispatch = useDispatch();
   
@@ -25,11 +27,29 @@ const Textbox = React.memo<ImStatelessProps>(({control}) => {
     ws.updateControlProps(payload);
   }
 
-  return <TextField value={control.value} onChange={handleChange} />;
+  const textFieldProps: ITextFieldProps = {
+    value: control.value ? control.value : "",
+    label: control.label ? control.label : null,
+    placeholder: control.placeholder ? control.placeholder : null,
+    errorMessage: control.errormessage ? control.errormessage : null,
+    description: control.description ? control.description : null,
+    multiline: control.multiline ? true : false,
+    type: control.password ? "password" : undefined,
+    canRevealPassword: control.password ? true : undefined,
+    required: control.required ? true : undefined,
+    disabled: disabled,
+    styles: {
+      root: {
+        width: control.width !== undefined ? control.width : undefined,
+        height: control.height !== undefined ? control.height : undefined,
+        padding: control.padding !== undefined ? control.padding : undefined,
+        margin: control.margin !== undefined ? control.margin : undefined  
+      },
+      field: {
+        textAlign: control.align !== undefined ? control.align : undefined,
+      },
+    }
+  };
+
+  return <TextField {...textFieldProps} onChange={handleChange} />;
 })
-
-interface ImStatelessProps {
-  control: any;
-}
-
-export default Textbox
