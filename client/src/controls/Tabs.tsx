cc4afcbda0614074238b9696751f58d44d171440
Feodor Fitsner <feodor@appveyor.com>
2021-01-24 13:23:30 -0800

    Fluent UI migrated to 8.0.0-beta.41

--- client/src/controls/Tabs.tsx
+++ client/src/controls/Tabs.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { WebSocketContext } from '../WebSocket';
 import { useDispatch, shallowEqual, useSelector } from 'react-redux'
 import { changeProps } from '../slices/pageSlice'
-import { Pivot, PivotItem, IPivotProps, PivotLinkFormat } from '@fluentui/react';
+import { Pivot, PivotItem, IPivotProps } from '@fluentui/react';
 import { IControlProps } from './IControlProps'
 import { ControlsList } from './ControlsList'
 
@@ -34,7 +34,7 @@ export const Tabs = React.memo<IControlProps>(({control, parentDisabled}) => {
   }
 
   const pivotProps: IPivotProps = {
-    linkFormat: control.solid === 'true' ? PivotLinkFormat.tabs : undefined,
+    linkFormat: control.solid === 'true' ? 'tabs' : undefined,
     styles: {
       root: {
         marginBottom: control.margin ? control.margin : undefined,
@@ -63,7 +63,7 @@ export const Tabs = React.memo<IControlProps>(({control, parentDisabled}) => {
   }
 
   return <Pivot {...pivotProps} onLinkClick={handleChange}>
-    {tabControls.map(tab =>
+    {tabControls.map((tab: any) =>
     <PivotItem key={tab.i} {...tab.props}>
       <ControlsList controls={tab.controls} parentDisabled={disabled} />
     </PivotItem>)}
