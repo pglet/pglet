29cd8048e832a0103a091a3818af689044249e37
Feodor Fitsner <feodor@appveyor.com>
2021-01-25 10:42:18 -0800

    Update ButtonSample.tsx

--- client/src/playground/ButtonSample.tsx
+++ client/src/playground/ButtonSample.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ContextualMenu } from '@fluentui/react';
+import { Text, ContextualMenu, IContextualMenuItem } from '@fluentui/react';
 import { MenuButton } from '@fluentui/react-button';
 import { Stack } from '@fluentui/react';
 
@@ -10,13 +10,21 @@ let menu = {
 };
 
 export const ButtonSample: React.FunctionComponent = () => {
+
+  // onItemClick?: (ev?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>, item?: IContextualMenuItem) => boolean | void;
+
+  const handleClick = (ev?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>, item?: IContextualMenuItem) => {
+    console.log(item);
+    return true;
+  }
+
   return (
     <div>
       <Stack horizontalAlign="stretch">
         <Stack horizontal horizontalAlign='space-between'>
           <Text>Left</Text>
           <Stack horizontal>
-            <MenuButton primary menu={<ContextualMenu {...menu} />}>A</MenuButton>
+            <MenuButton primary menu={<ContextualMenu {...menu} onItemClick={handleClick} />}>A</MenuButton>
           </Stack>
         </Stack>
       </Stack>
