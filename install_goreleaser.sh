66f737b66f5512a247957791f846d21d8f9466fb
Feodor Fitsner <feodor@appveyor.com>
2021-01-29 14:43:19 -0800

    ls -al /usr/local

--- install_goreleaser.sh
+++ install_goreleaser.sh
@@ -1,2 +1,3 @@
 curl -fsSL https://appveyordownloads.blob.core.windows.net/misc/goreleaser-m1.tar.gz -o /tmp/goreleaser-m1.tar.gz
 sudo tar zxf /tmp/goreleaser-m1.tar.gz -C /usr/local
+ls -al /usr/local
