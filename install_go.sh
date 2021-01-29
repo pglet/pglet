313d7a838afabbff8973c541fcaccb047ab28e8d
Feodor Fitsner <feodor@appveyor.com>
2021-01-29 12:15:18 -0800

    Always use GVM to install Go on linux/mac

--- install_go.sh
+++ install_go.sh
@@ -1,14 +1,11 @@
-# GO_OS=linux
-# if [[ "$OSTYPE" == "darwin"* ]]; then
-#     GO_OS=darwin
-# fi
+GO_OS=linux
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    GO_OS=darwin
+fi
 
-# curl -fsSL "https://golang.org/dl/go${GO_VERSION}.${GO_OS}-amd64.tar.gz" -o /tmp/go-linux-amd64.tar.gz
-# sudo tar zxf /tmp/go-linux-amd64.tar.gz -C /usr/local
-# export PATH=/usr/local/go/bin:$PATH
-# export GOROOT=/usr/local/go
-# export GOPATH=$HOME/go
-source  ~/.gvm/scripts/gvm
-gvm install go${GO_VERSION} -B
-gvm use go${GO_VERSION}
+curl -fsSL "https://golang.org/dl/go${GO_VERSION}.${GO_OS}-amd64.tar.gz" -o /tmp/go-linux-amd64.tar.gz
+sudo tar zxf /tmp/go-linux-amd64.tar.gz -C /usr/local
+export PATH=/usr/local/go/bin:$PATH
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
 go version
