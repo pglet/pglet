3cb3d07f6ea1de130897cc91338eac2fa1ed41ea
Feodor Fitsner <feodor@appveyor.com>
2021-02-01 11:41:20 -0800

    Theme builder

--- client/src/controls/Page.tsx
+++ client/src/controls/Page.tsx
@@ -2,11 +2,17 @@ import React, { useEffect, useContext } from 'react'
 import { shallowEqual, useSelector } from 'react-redux'
 import { ControlsList } from './ControlsList'
 import useTitle from '../hooks/useTitle'
-import { Stack, IStackProps, IStackTokens } from '@fluentui/react';
+import { Stack, IStackProps, IStackTokens, createTheme, ThemeProvider } from '@fluentui/react';
+import {
+  BaseSlots,
+  ThemeGenerator,
+  themeRulesStandardCreator,
+} from '@fluentui/react/lib/ThemeGenerator';
+import { isDark } from '@fluentui/react/lib/Color';
 import { IPageProps } from './IPageProps'
 import { WebSocketContext } from '../WebSocket';
 
-export const Page = React.memo<IPageProps>(({control, pageName}) => {
+export const Page = React.memo<IPageProps>(({ control, pageName }) => {
 
   //console.log(`render page: ${control.i}`);
 
@@ -18,6 +24,56 @@ export const Page = React.memo<IPageProps>(({control, pageName}) => {
     title = control.title
   }
   useTitle(title)
+
+  // theme
+  const themePrimaryColor = control.themeprimarycolor ? control.themeprimarycolor : '#8e16c9'
+  const themeTextColor = control.themetextcolor ? control.themetextcolor : '#323130'
+  const themeBackgroundColor = control.themebackgroundcolor ? control.themebackgroundcolor : '#ffffff'  
+
+  //console.log("themeBackgroundColor:", themeBackgroundColor);  
+
+  // theme
+  let themeRules = themeRulesStandardCreator();
+  function changeColor(baseSlot: BaseSlots, newColor: any) {
+    const currentIsDark = isDark(themeRules[BaseSlots[BaseSlots.backgroundColor]].color!);
+    ThemeGenerator.setSlot(themeRules[BaseSlots[baseSlot]], newColor, currentIsDark, true, true);
+    if (currentIsDark !== isDark(themeRules[BaseSlots[BaseSlots.backgroundColor]].color!)) {
+      // isInverted got swapped, so need to refresh slots with new shading rules
+      ThemeGenerator.insureSlots(themeRules, currentIsDark);
+    }
+  }
+
+  changeColor(BaseSlots.primaryColor, themePrimaryColor);
+  changeColor(BaseSlots.backgroundColor, themeBackgroundColor);
+  changeColor(BaseSlots.foregroundColor, themeTextColor);
+  changeColor(BaseSlots.backgroundColor, themeBackgroundColor);
+
+  const themeAsJson: {
+    [key: string]: string;
+  } = ThemeGenerator.getThemeAsJson(themeRules);
+
+  const finalTheme = createTheme({
+    ...{ palette: themeAsJson },
+    isInverted: isDark(themeRules[BaseSlots[BaseSlots.backgroundColor]].color!),
+  });
+
+  // strip out the unnecessary shade slots from the final output theme
+  // const abridgedTheme: IThemeRules = {};
+  // for (const ruleName in themeRules) {
+  //   if (themeRules.hasOwnProperty(ruleName)) {
+  //     if (
+  //       ruleName.indexOf('ColorShade') === -1 &&
+  //       ruleName !== 'primaryColor' &&
+  //       ruleName !== 'backgroundColor' &&
+  //       ruleName !== 'foregroundColor' &&
+  //       ruleName.indexOf('body') === -1
+  //     ) {
+  //       abridgedTheme[ruleName] = themeRules[ruleName];
+  //     }
+  //   }
+  // }
+
+  // const jsonTheme = JSON.stringify(ThemeGenerator.getThemeAsJson(abridgedTheme), undefined, 2)
 
   useEffect(() => {
     // https://danburzo.github.io/react-recipes/recipes/use-effect.html
@@ -39,7 +95,7 @@ export const Page = React.memo<IPageProps>(({control, pageName}) => {
 
   // stack props
   const stackProps: IStackProps = {
-    verticalFill: control.verticalfill ? control.verticalfill : false,
+    verticalFill: control.verticalfill ? control.verticalfill : true,
     horizontalAlign: control.horizontalalign === '' ? undefined : (control.horizontalalign ? control.horizontalalign : "start"),
     verticalAlign: control.verticalalign === '' ? undefined : (control.verticalalign ? control.verticalalign : "start"),
     styles: {
@@ -56,7 +112,11 @@ export const Page = React.memo<IPageProps>(({control, pageName}) => {
     childrenGap: control.gap ? control.gap : 10
   }
 
-  return <Stack tokens={stackTokens} {...stackProps}>
-    <ControlsList controls={childControls} parentDisabled={disabled} />
-  </Stack>
+  document.documentElement.style.background = themeBackgroundColor
+
+  return <ThemeProvider theme={finalTheme}>
+    <Stack tokens={stackTokens} {...stackProps}>
+      <ControlsList controls={childControls} parentDisabled={disabled} />
+    </Stack>
+  </ThemeProvider>
 })
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { mergeStyles, loadTheme, initializeIcons } from '@fluentui/react';
+import { mergeStyles, initializeIcons  } from '@fluentui/react';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import rootReducer from './rootReducer'
@@ -20,32 +20,6 @@ mergeStyles({
     }
   }
 });
-
-loadTheme({
-  palette: {
-    themePrimary: '#8e16c9',
-    themeLighterAlt: '#faf4fd',
-    themeLighter: '#ebd3f6',
-    themeLight: '#daafef',
-    themeTertiary: '#b768df',
-    themeSecondary: '#9a2dd0',
-    themeDarkAlt: '#8014b5',
-    themeDark: '#6c1199',
-    themeDarker: '#4f0c71',
-    neutralLighterAlt: '#faf9f8',
-    neutralLighter: '#f3f2f1',
-    neutralLight: '#edebe9',
-    neutralQuaternaryAlt: '#e1dfdd',
-    neutralQuaternary: '#d0d0d0',
-    neutralTertiaryAlt: '#c8c6c4',
-    neutralTertiary: '#a19f9d',
-    neutralSecondary: '#605e5c',
-    neutralPrimaryAlt: '#3b3a39',
-    neutralPrimary: '#323130',
-    neutralDark: '#201f1e',
-    black: '#000000',
-    white: '#ffffff',
-  }});
 
 const store = configureStore({
   reducer: rootReducer
