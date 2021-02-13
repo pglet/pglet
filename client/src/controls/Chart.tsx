7e41781273568ccafe9772f7e8dce18f7b996f8d
Feodor Fitsner <feodor@appveyor.com>
2021-02-13 14:09:37 -0800

    vertical bar chart

--- client/src/controls/Chart.tsx
+++ client/src/controls/Chart.tsx
@@ -5,7 +5,36 @@ import { IControlProps, defaultPixels } from './IControlProps'
 
 export const Chart = React.memo<IControlProps>(({control, parentDisabled}) => {
 
+  const [dimensions, setDimensions] = React.useState({ 
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+
+  React.useEffect(() => {
+
+    let resizeTimeout:any = null;
+    function handleResize() {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(() => {
+        setDimensions({
+          height: window.innerHeight,
+          width: window.innerWidth
+        })
+      }, 500)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const chartProps: IVerticalBarChartProps = {
+    hideLegend: control.legend !== 'true',
+    hideTooltip: control.tooltips !== 'true',
+    yMinValue: control.ymin !== undefined ? parseFloat(control.ymin) : undefined,
+    yMaxValue: control.ymax !== undefined ? parseFloat(control.ymax) : undefined,
+    yAxisTickCount: control.yticks !== undefined ? parseInt(control.yticks) : 1,
+    height: dimensions.height,
+    width: dimensions.width,
     styles: {
       root: {
         width: control.width !== undefined ? defaultPixels(control.width) : undefined,
@@ -24,12 +53,19 @@ export const Chart = React.memo<IControlProps>(({control, parentDisabled}) => {
         points: data.c.map((childId: any) => state.page.controls[childId])
       })
     );
-  }, shallowEqual);  
+  }, shallowEqual);
 
   //console.log(data);
 
   if (data.length > 0) {
     chartProps.data = data[0].points;
+    const yvals = chartProps.data!.map(p => p.y);
+    if (chartProps.yMinValue === undefined) {
+      chartProps.yMinValue = Math.min(...yvals)
+    }    
+    if (chartProps.yMaxValue === undefined) {
+      chartProps.yMaxValue = Math.max(...yvals)
+    }
   }
 
   return (
