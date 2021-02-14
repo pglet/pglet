1d71ba2b425aa05b05facbe739e4c5688c11ef24
Feodor Fitsner <feodor@appveyor.com>
2021-02-13 20:13:48 -0800

    VerticalBar chart complete

--- client/src/controls/Chart.tsx
+++ client/src/controls/Chart.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallowEqual, useSelector } from 'react-redux'
 import { VerticalBarChart, IVerticalBarChartProps } from '@fluentui/react-charting';
+import * as d3 from 'd3-format';
 import { IControlProps, defaultPixels } from './IControlProps'
 
 export const Chart = React.memo<IControlProps>(({control, parentDisabled}) => {
@@ -30,9 +31,12 @@ export const Chart = React.memo<IControlProps>(({control, parentDisabled}) => {
   const chartProps: IVerticalBarChartProps = {
     hideLegend: control.legend !== 'true',
     hideTooltip: control.tooltips !== 'true',
+    barWidth: control.barwidth !== undefined ? parseInt(control.barwidth) : undefined,
+    colors: control.colors !== undefined ? control.colors.split(/[ ,]+/g) : undefined,
     yMinValue: control.ymin !== undefined ? parseFloat(control.ymin) : undefined,
     yMaxValue: control.ymax !== undefined ? parseFloat(control.ymax) : undefined,
     yAxisTickCount: control.yticks !== undefined ? parseInt(control.yticks) : 1,
+    yAxisTickFormat: control.yformat !== undefined ? d3.format(control.yformat) : undefined,
     height: dimensions.height,
     width: dimensions.width,
     styles: {
@@ -50,7 +54,17 @@ export const Chart = React.memo<IControlProps>(({control, parentDisabled}) => {
     .filter((c: any) => c.t === 'data').map((data: any) => 
       ({
         ...data,
-        points: data.c.map((childId: any) => state.page.controls[childId])
+        points: data.c.map((childId: any) => {
+          const p = state.page.controls[childId];
+          return {
+            x: p.x,
+            y: parseFloat(p.y),
+            legend: p.legend,
+            color: p.color,
+            xAxisCalloutData: p.xtooltip,
+            yAxisCalloutData: p.ytooltip
+          }
+        })
       })
     );
   }, shallowEqual);
@@ -60,9 +74,6 @@ export const Chart = React.memo<IControlProps>(({control, parentDisabled}) => {
   if (data.length > 0) {
     chartProps.data = data[0].points;
     const yvals = chartProps.data!.map(p => p.y);
-    if (chartProps.yMinValue === undefined) {
-      chartProps.yMinValue = Math.min(...yvals)
-    }    
     if (chartProps.yMaxValue === undefined) {
       chartProps.yMaxValue = Math.max(...yvals)
     }
