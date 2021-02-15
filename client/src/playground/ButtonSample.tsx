c40090e9f2116ac546410911acb5365932465a4f
Feodor Fitsner <feodor@appveyor.com>
2021-02-15 13:50:00 -0800

    Button examples removed

--- client/src/playground/ButtonSample.tsx
+++ client/src/playground/ButtonSample.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Text, ContextualMenu, IContextualMenuItem } from '@fluentui/react';
-import { MenuButton } from '@fluentui/react-button';
+import { Text } from '@fluentui/react';
 import { Stack } from '@fluentui/react';
 //import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 //import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -18,20 +17,7 @@ import gfm from 'remark-gfm'
 // SyntaxHighlighter.registerLanguage('python', python);
 // SyntaxHighlighter.registerLanguage('bash', bash);
 
-let menu = {
-  items: [
-    { key: 'a', text: 'aaaaa' }
-  ]
-};
-
 export const ButtonSample: React.FunctionComponent = () => {
-
-  // onItemClick?: (ev?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>, item?: IContextualMenuItem) => boolean | void;
-
-  const handleClick = (ev?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>, item?: IContextualMenuItem) => {
-    console.log(item);
-    return true;
-  }
 
   const markdown = `
 
@@ -95,7 +81,6 @@ Pretty neat, eh?`
         <Stack horizontal horizontalAlign='space-between'>
           <Text>Left</Text>
           <Stack horizontal>
-            <MenuButton primary menu={<ContextualMenu {...menu} onItemClick={handleClick} />}>A</MenuButton>
           </Stack>
         </Stack>
       </Stack>
