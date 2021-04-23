bd233b59f477d663fc4274621d41e3131ce182a2
Feodor Fitsner <feodor@appveyor.com>
2021-04-23 16:02:20 -0700

    Update Spinner.tsx

--- client/src/controls/Spinner.tsx
+++ client/src/controls/Spinner.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Spinner, ISpinnerProps } from '@fluentui/react';
+import { Spinner, ISpinnerProps, SpinnerSize } from '@fluentui/react';
 import { IControlProps } from './Control.types'
 import { defaultPixels } from './Utils'
 
 export const MySpinner = React.memo<IControlProps>(({control}) => {
 
+  let size: SpinnerSize = SpinnerSize.medium;
+
+  switch (control.size ? control.size.toLowerCase() : '') {
+    case 'xsmall': size = SpinnerSize.xSmall; break;
+    case 'small': size = SpinnerSize.small; break;
+    case 'medium': size = SpinnerSize.medium; break;
+    case 'large': size = SpinnerSize.large; break;
+  }
+
   const spinnerProps: ISpinnerProps = {
     label: control.label ? control.label : null,
-    labelPosition: control.labelposition ? control.labelposition : null,
+    labelPosition: control.labelposition ? control.labelposition.toLowerCase() : null,
+    size: size,
     styles: {
       root: {
         width: control.width !== undefined ? defaultPixels(control.width) : undefined,
