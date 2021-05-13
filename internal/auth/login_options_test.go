8ce8cc238f66b977eed70b06d8aa1636ceed43f2
Feodor Fitsner <feodor@appveyor.com>
2021-05-12 17:22:10 -0700

    Fix tests

--- internal/auth/login_options_test.go
+++ internal/auth/login_options_test.go
@@ -14,15 +14,15 @@ func TestLoginOptions(t *testing.T) {
 		expected    *LoginOptions // expected result
 	}{
 		{"", nil},
-		{"*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: false}},
-		{"john@smith.com, jack@bauer.com", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: false}},
+		{"*", &LoginOptions{GitHubEnabled: true, AzureEnabled: true, GoogleEnabled: true}},
+		{"john@smith.com, jack@bauer.com", &LoginOptions{GitHubEnabled: true, AzureEnabled: true, GoogleEnabled: true}},
 		{"azure:john@smith.com", &LoginOptions{GitHubEnabled: false, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: false}},
-		{"*/*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: true, AzureEnabled: true, AzureGroupScope: true}},
+		{"*/*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: true, AzureEnabled: true, AzureGroupScope: true, GoogleEnabled: true, GoogleGroupScope: true}},
 		{"github:*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: false, AzureEnabled: false, AzureGroupScope: false}},
 		{"github:pglet/devops", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: true, AzureEnabled: false, AzureGroupScope: false}},
 		{"azure:*", &LoginOptions{GitHubEnabled: false, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: false}},
 		{"azure:*/*", &LoginOptions{GitHubEnabled: false, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: true}},
-		{"*, azure:*/*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: true}},
+		{"*, azure:*/*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: false, AzureEnabled: true, AzureGroupScope: true, GoogleEnabled: true}},
 		{"azure:*, github:*/*", &LoginOptions{GitHubEnabled: true, GitHubGroupScope: true, AzureEnabled: true, AzureGroupScope: false}},
 	}
 
